d4b343640be1a253c43a4f6ef2216a1a1f926af8
alexsandropancracio <alexsandropancracioofficial@gmail.com>
2025-01-31 22:28:42 -0300

    Curso JS Atualizado

--- JAVASCRIPT/dom.js
+++ JAVASCRIPT/dom.js
@@ -1,37 +1,54 @@
-const dc1 = document.getElementById('c1')//aqui eu chamo o HTML através do ID.
-const dc2 = document.getElementById('c2')
-const dc3 = document.getElementById('c3')
-const dc4 = document.getElementById('c4')
-const dc5 = document.getElementById('c5')
-const dc6 = document.getElementById('c6')
+//Relação dos elementos DOM
 
-const arrayElementos = [dc1, dc2, dc3, dc4, dc5, dc6]//podemos armazenar os elementos dentro de uma array e desta maneira podemos usar diversos métodos.
+const caixa = document.querySelector('#caixa')
+const box = [...document.querySelectorAll('.curso')]
 
-// for(x of arrayElementos){
-//     x.innerHTML = 'Curso de Javascript'
-// }
+console.log(caixa.children)//Quando é uma propriedade, não precisa do parênteses, quando é um método, precisa ser usado o parênteses. Dessa forma também, eu consigo pegar o "elemento filho" da variável que armazena a div do id caixa.
 
-//arrayElementos.map((e)=>{
-    //e.innerHTML = "Curso de Javascript"
-    //console.log(e)
-//})podemos operar todos os elementos da array com o map e para percorrer, iterar e modificar os elementos de uma array, de preferência usamos o método map que é mais simples e mais moderna.
+console.log(caixa.children[0])//Além disso, indico qual elemento eu posso pegar usando os colchetes.
 
-console.log(dc1)//pra imprimir o a tag mais o elemento HTML.
-console.log(dc1.id)//pra imprimir o id do elemento.
-console.log(dc1.innerHTML)//pra imprimir o elemento HTML.
+console.log(caixa.firstElementChild)//Assim como vemos no console, o firtElementChild serve pra pegar o primeiro elemento do parent element.
 
-//dc1.innerHTML = "Curso de JS"//posso também mudar o elemento do HTML.
+console.log(caixa.children[caixa.children.length-1])//De forma específica, fazemos assim para apontar o último elemento dentro do parent element.
 
-const colecaoHTML = [...document.getElementsByTagName('div')]//peguei o retorno do HTMLcolection e expalhei com o método spreed de uma forma menos direta.
-//Mas eu poderia transformar a constante em um variável let e fazer da segunte maneira:
+console.log(caixa.lastElementChild)//Diretamente, podemos também dessa forma pegar o último elemento do parent element.
 
-/*
+console.log(caixa.getRootNode())//Assim, além de termos o parent element, podemos saber também qual é o elemento raiz do parent element, ou seja, o rootNode
 
-let colecaoHTML = document.getElementsByTagName('div')
-colecaoHTML = [...colecaoHTML]
+console.log(caixa.hasChildNodes())//Com esse método eu verifico se o elemento possiu "filhos".
 
-*/
+console.log(box[0].hasChildNodes())//Dessa forma que verifico também se um conjunto de elementos tem filhos.
+
+if(box[0].childNodes.length > 0){
+    console.log('Possui elementos!')
+} else {
+    console.log('Não possui elementos.')
+}
+
+if(box[0].children.length > 0){
+    console.log('Possui elementos HTML!')
+} else {
+    console.log('Não possui elementos HTML.')
+}
+
+if(caixa.childNodes.length > 0){
+    console.log('Possui filhos!')
+} else {
+    console.log('Não possui filhos.')
+}
+
+console.log(caixa.children[1].innerHTML)//Da mesma forma que determino o índice com a array do box, assim faço também com a div.
+
+console.log(box[0].childNodes)//Assim verificamos o que há dentro da tag, seja um elemento HTML, seja um comentário, seja um texto ou qualquer outra coisa.
+
+console.log(box[0].children)//Dessa forma verificamos se há somente elementos HTML.
+
+box.forEach((element)=>{
+    console.log(element)
+})
+
+for(const index of box){//Desse forma como acima, iteramos um um array.
+    console.log(index)
+}
 
 
-//console.log(arrayElementos)
-console.log(colecaoHTML)
